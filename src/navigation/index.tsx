fb63ebfd76ecefe895eef553e37abbcac10723b7
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2024-09-08 16:43:04 +0200

    feat: set up settings screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,15 +1,56 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useSetAtom } from 'jotai';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import type {
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
+
+import { PressableScale } from '../animations/action-tray/components/TouchableScale';
 
 import { Home } from './home';
 import { Screens } from './screens';
 import { SearchFilterAtom } from './states/filters';
+import { Settings } from './settings';
 
 const MainStack = createNativeStackNavigator();
 
 const MainNavigation = React.memo(() => {
   const updateFilterLabel = useSetAtom(SearchFilterAtom);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { navigate } = useNavigation<any>();
+
+  const navigateToSettings = useCallback(() => {
+    navigate('Settings');
+  }, [navigate]);
+
+  const headerRight = useMemo(() => {
+    return (
+      <PressableScale
+        onPress={navigateToSettings}
+        style={{
+          padding: 10,
+        }}>
+        <Ionicons name="settings-outline" size={24} color="white" />
+      </PressableScale>
+    );
+  }, [navigateToSettings]);
+  const options = useMemo(() => {
+    return {
+      ...HomeHeaderOptions,
+      headerSearchBarOptions: {
+        ...HomeHeaderOptions.headerSearchBarOptions,
+        onChangeText: (
+          event: NativeSyntheticEvent<TextInputChangeEventData>,
+        ) => {
+          updateFilterLabel(event.nativeEvent.text);
+        },
+      },
+      headerRight,
+    };
+  }, [headerRight, updateFilterLabel]);
 
   return (
     <MainStack.Navigator
@@ -17,32 +58,13 @@ const MainNavigation = React.memo(() => {
       screenOptions={{
         headerShown: false,
       }}>
+      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+      <MainStack.Screen name="Home" component={Home} options={options as any} />
       <MainStack.Screen
-        name="Home"
-        component={Home}
+        name="Settings"
+        component={Settings}
         options={{
-          headerShown: true,
-          headerTransparent: true,
-          headerLargeTitle: true,
-          headerSearchBarOptions: {
-            placeholder: 'Search',
-            tintColor: 'white',
-            textColor: 'white',
-            hintTextColor: 'rgba(255,255,255,0.8)',
-            onChangeText: event => {
-              updateFilterLabel(event.nativeEvent.text);
-            },
-            inputType: 'text',
-            obscureBackground: false,
-          },
-          headerBlurEffect: 'systemThinMaterialDark',
-          headerStyle: {
-            backgroundColor: 'transparent',
-          },
-          title: 'Demos',
-          headerTitleStyle: {
-            color: 'white',
-          },
+          presentation: 'modal',
         }}
       />
       {Screens.map(screen => {
@@ -57,5 +79,27 @@ const MainNavigation = React.memo(() => {
     </MainStack.Navigator>
   );
 });
+
+const HomeHeaderOptions = {
+  headerShown: true,
+  headerTransparent: true,
+  headerLargeTitle: true,
+  headerSearchBarOptions: {
+    placeholder: 'Search',
+    tintColor: 'white',
+    textColor: 'white',
+    hintTextColor: 'rgba(255,255,255,0.8)',
+    inputType: 'text',
+    obscureBackground: false,
+  },
+  headerBlurEffect: 'systemThinMaterialDark',
+  headerStyle: {
+    backgroundColor: 'transparent',
+  },
+  title: 'Demos',
+  headerTitleStyle: {
+    color: 'white',
+  },
+} as const;
 
 export { MainNavigation };
