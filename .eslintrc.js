521811f28b0b7be94ef90ea0e9439db548fe6b11
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-09-21 12:55:20 +0200

    chore(eslint): update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  extends: ['react-native-wcandillon', 'prettier'],
-  plugins: [
-    // ... other plugins
-    'prettier',
-    'jest',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'jest', 'import', 'react', 'react-hooks', 'react-native'],
   ignorePatterns: [
     'node_modules/',
     '**/node_modules/',
@@ -23,10 +24,31 @@ module.exports = {
     'import/no-useless-path-segments': 'off',
     'no-unused-vars': 'off',
     'prettier/prettier': 'error',
-    '@typescript-eslint/consistent-type-imports': 'off', // Disable problematic rule temporarily
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
   },
   env: {
-    // ... other environments
     'jest/globals': true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
