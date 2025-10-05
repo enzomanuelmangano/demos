8f3d8c775f4df4ce69bc2b1d49a6f9ffed4a164b
Enzo Manuel Mangano <37193302+enzomanuelmangano@users.noreply.github.com>
2025-10-05 21:51:33 +0200

    feat: liquid glass resolver babel plugin (#5)
    * feat: babel liquid plugin resolver
    
    * fix: remove useless metro customize
    
    * feat: update babel config to use liquid-glass-resolver plugin
    
    * refactor(home-screen): replace LiquidButton with StaggeredText component and remove LiquidButton files
    
    * feat: add AnimatedSlider component and update dependencies
    
    * refactor(animated-slider): prefer using animated props
    
    * fix: disable liquid glass on expo go
    
    * refactor: imports
    
    * refactor(animated-slider): optimize onValueChange handler and improve styling
    
    * refactor(babel-config): improve liquid-glass-resolver plugin
    
    * feat(qrcode): replace SweepGradient with LinearGradient
    
    * feat(liquid-glass-playground): add new Playground component and update @expo/ui dependency
    
    * refactor(registry): clean up imports
    
    * feat(liquid-glass-playground): handle blurView
    
    * feat(liquid-glass-resolver): prioritize iOS-specific file handling and add Overlay component
    
    * fix(babel-config): disable liquid glass feature based on environment variable
    
    * chore(dependencies): update @expo/ui
    
    * chore(easignore): remove scripts entry from .easignore
    
    * chore(eas.json): add environment variable for liquid glass feature in production
    
    * fix(overlay): remove background color from Overlay component for improved styling

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-worklets/plugin'],
+    plugins: [
+      'react-native-worklets/plugin',
+      [
+        './plugins/liquid-glass-resolver',
+        {
+          // Customize the suffix used for variant files
+          // Default: '.liquid' (e.g., component.liquid.tsx)
+          suffix: '.liquid',
+
+          // Enable debug logging to see transformation details
+          debugLogging: false,
+
+          disabled: process.env.EXPO_PUBLIC_LIQUID_GLASS_ENABLED !== 'true',
+        },
+      ],
+    ],
   };
 };
