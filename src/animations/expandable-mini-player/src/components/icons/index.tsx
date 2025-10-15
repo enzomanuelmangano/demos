e3f1b0a5bfdd33ce2455efbe835fe9f166f1ee54
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-10-15 21:37:13 +0200

    refactor: reorder imports in icon component for improved clarity

--- src/animations/expandable-mini-player/src/components/icons/index.tsx
+++ src/animations/expandable-mini-player/src/components/icons/index.tsx
@@ -1,6 +1,6 @@
-import type { ComponentProps } from 'react';
-
 import { Ionicons } from '@expo/vector-icons';
+
+import type { ComponentProps } from 'react';
 
 type IconProps = Omit<ComponentProps<typeof Ionicons>, 'name' | 'size'>;
 
