cb0293dde84cebcf7290c1abad743ea5eccd5c71
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-12-07 12:59:23 +0100

    fix: glitchy online-to-offline animation

--- src/animations/online-offline/index.tsx
+++ src/animations/online-offline/index.tsx
@@ -135,7 +135,6 @@ export const OnlineOffline = () => {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     backgroundColor: '#fff',
     flex: 1,
     justifyContent: 'center',
